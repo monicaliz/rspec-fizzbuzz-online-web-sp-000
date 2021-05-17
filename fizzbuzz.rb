119e8ab2aad2fe36a85cd86675605181874bb44a
Monica Liz Rodriguez <monica.liz.rodriguez@gmail.com>
2021-05-17 23:18:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz
+# if fizz_3 == 3
+#   puts "Fizz"
+# elsif condition
+  
+
+
+
+end
